3515c052ee3b6a10b1a84936b88beb8a664801e2
stanley Yin <stanley6140992@gmail.com>
2022-03-18 16:19:01 +0800

    修改排序跟搜尋功能

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const express = require("express"); // 引入express
 const cors = require("cors"); // CORS
 const db = require("./connect-mysql"); //
 const data = require("./players.json");
+const session = require("express-session");
+const MysqlStore = require("express-mysql-session")(session); // 套件能夠將sess 資料儲存進mysql
+const sessionStore = new MysqlStore({}, db);
 
 const app = express();
 
@@ -19,7 +22,27 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// session設定
+app.use(
+  session({
+    saveUninitialized: false, // 還沒用到時，是否要儲存
+    resave: false, // 沒變更內容是否要強制儲存
+    secret: "mySecret",
+    store: sessionStore,
+    cookie: {
+      maxAge: 120000,
+    },
+  })
+);
+
 // === 路由定義 =========================================
+
+app.get("/try-sess", (req, res) => {
+  req.session.myVar = req.session.myVar || 0; // myVar是自己定義的變數
+  req.session.myVar++;
+  console.log(req.session);
+  res.send(req.session);
+});
 
 // 1. getData API
 app.get("/player-list", async (req, res) => {
@@ -27,7 +50,7 @@ app.get("/player-list", async (req, res) => {
   let team = req.query.team;
   let keyword = req.query.keyword;
   let sortItem = req.query.sortItem;
-  let sortBy = req.query.sortBy;
+  let sortBy = req.query.sortBy?req.query.sortBy:'DESC';
 
   let output = {};
 
@@ -36,15 +59,15 @@ app.get("/player-list", async (req, res) => {
   let limit = ` LIMIT ${(page - 1) * 15},${perPage}`;
 
   let where = "WHERE 1 ";
-  let ORDER = 'ORDER BY points_per_game DESC'
+  let ORDER = "ORDER BY points_per_game DESC";
   if (team) {
     where += `AND team_name = '${team}'`;
   }
   if (keyword) {
-    where += `AND name LIKE '${keyword}%'`;
+    where += `AND name LIKE '%${keyword}%'`;
   }
-  if(sortItem){
-    ORDER = ` ORDER BY ${sortItem} DESC`
+  if (sortItem) {
+    ORDER = ` ORDER BY ${sortItem} ${sortBy}`;
   }
   // 全部符合的資料
   const sql1 = `SELECT * FROM players ${where} `;
@@ -67,32 +90,30 @@ app.get("/player-list", async (req, res) => {
     allData: r1,
     limitData: r2,
   };
-  res.send(output);
+  res.json(output);
 });
 
 // 2. player-details API
-app.get('/player-list/:sid',async(req,res)=>{
-  const sid = req.params.sid
+app.get("/player-list/:sid", async (req, res) => {
+  const sid = req.params.sid;
   // console.log(sid)
 
-  const sql = `SELECT * FROM players WHERE sid = '${sid}'`
-  const [r] = await db.query(sql)
+  const sql = `SELECT * FROM players WHERE sid = '${sid}'`;
+  const [r] = await db.query(sql);
 
-  if(r.affectedRows !==0){
-    res.send(r)
+  if (r.affectedRows !== 0) {
+    res.send(r);
   }
   // console.log(r)
-})
+});
 
 // 3. team-chart API
-app.get('/team-chart',async(req,res)=>{
-  const sql = `SELECT team_acronym , COUNT(1) as teamSize FROM players GROUP BY team_acronym`
-  const [r] = await db.query(sql)
+app.get("/team-chart", async (req, res) => {
+  const sql = `SELECT team_acronym , COUNT(1) as teamSize FROM players GROUP BY team_acronym`;
+  const [r] = await db.query(sql);
 
-  res.send(r)
-})
-
-
+  res.send(r);
+});
 
 // === port 設定 =========================================
 let port = process.env.PORT;
